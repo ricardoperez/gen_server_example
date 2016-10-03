3d9332e471e8e4b3a2555d1687c28bea96e1d0a8
Ricardo Perez <ricardop@square-enix.com>
2016-10-03 12:05:47 +0100

    change variable names

--- lib/game_sheet.ex
+++ lib/game_sheet.ex
@@ -5,24 +5,24 @@ defmodule GenServerExample.GameSheet do
     GenServer.start_link(__MODULE__, itens)
   end
 
-  def add_item(pid, item_name, item) do
-    GenServer.cast pid, {:add_to_item, item_name, item}
+  def add_item(pid, row_handler, item) do
+    GenServer.cast pid, {:add_item_to_row, row_handler, item}
   end
 
-  def show_items(pid, item) do
-    GenServer.cast pid, {:result, item}
+  def show_items_in_row(pid, row_handler) do
+    GenServer.cast pid, {:result, row_handler}
   end
 
    def init(itens) do
      {:ok, itens}
    end
 
-  def handle_cast({:add_to_item, item_name, item}, itens) do
-    {:noreply, Map.update(itens, item_name, 0, fn(_) -> List.insert_at itens[:carros], -1, item  end )}
+  def handle_cast({:add_item_to_row, row_handler, item}, itens) do
+    {:noreply, Map.update(itens, row_handler, 0, fn(_) -> List.insert_at itens[row_handler], -1, item  end )}
   end
 
-  def handle_cast({:result, item}, itens) do
-    IO.puts itens[item]
+  def handle_cast({:result, row_handler}, itens) do
+    IO.puts itens[row_handler]
     {:noreply, itens}
   end
 end
